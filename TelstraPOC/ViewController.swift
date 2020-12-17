3142a167cf8d56e76eb410c89b84196c2606a295
AnilGupta85 <gupta.anil85@gmail.com>
2020-12-17 23:06:51 +1100

    Added local project folder to Git repository

---
+++ TelstraPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelstraPOC
+//
+//  Created by Anil Gupta on 17/12/20.
+//  Copyright © 2020 Anil Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
