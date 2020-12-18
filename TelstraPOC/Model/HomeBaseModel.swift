ca7e0ef81e8e59e62ba3cfb4109c86a3b262fa2a
AnilGupta85 <gupta.anil85@gmail.com>
2020-12-18 17:34:09 +1100

    Added MVVM based folder and classes i.e. HomeTableViewCell, HomeViewModel, HomeBaseModel and RowsDataModel

---
+++ TelstraPOC/Model/HomeBaseModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeBaseModel.swift
+//  TelstraPOC
+//
+//  Created by Anil Gupta on 18/12/20.
+//  Copyright © 2020 Anil Gupta. All rights reserved.
+//
+
+import Foundation
---
+++ TelstraPOC/Model/RowsDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  RowsDataModel.swift
+//  TelstraPOC
+//
+//  Created by Anil Gupta on 18/12/20.
+//  Copyright © 2020 Anil Gupta. All rights reserved.
+//
+
+import Foundation
